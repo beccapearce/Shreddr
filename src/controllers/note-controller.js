dfca726051599e5d59a56a883dae1fc006eb82d0
lukew244 <luke.webster86@gmail.com>
2016-08-17 16:20:23 +0100

    Integrates form

--- src/controllers/note-controller.js
+++ src/controllers/note-controller.js
@@ -1,15 +1,76 @@
 (function(exports) {
 
   function NoteController() {
-    var newNoteList = new NoteList();
-    var newNoteListView = new NoteListView(newNoteList);
+    var notelistcontroller = new NoteList();
+    document.getElementById('submit').addEventListener('click', function() {
+      NoteController.writeNote();
+    });
+
+
+    // var newNoteListView = new NoteListView(newNoteList);
   }
 
-    NoteController.prototype.renderPage = function() {
-      element = document.getElementById("app");
-      element.innerHTML = newNoteListView.render();
-    };
+
+  NoteController.prototype.writeNote = function() {
+    var text = document.getElementById('notebook').value;
+    notelistcontroller.createNote(text);
+    var viewdanotes = new NoteListView(notelistcontroller);
+    var element = document.getElementById('listofnotes');
+    element.innerHTML = viewdanotes.render();
+    document.getElementById('notebook').value = '';
+
+  };
+
+
 
 exports.NoteController = NoteController;
 
 })(this);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // document.getElementById('submit').addEventListener('click', function() {
+  //   var text = document.getElementById('notebook').value;
+  //   notelistcontroller.createNote(text);
+  //   updateNoteList();
+  // });
+  //
+  //
+  //   // NoteController.prototype.renderPage = function() {
+  //   //   element = document.getElementById("app");
+  //   //   element.innerHTML = newNoteListView.render();
+  //   // };
+  //
+  //   // NoteController.prototype.writeNote = function() {
+  //   //
+  //   //   });
+  //   // };
+  //
+  //     NoteController.prototype.updateNoteList = function () {
+  //       var viewdanotes = new NoteListView(notelistcontroller);
+  //       var element = document.getElementById('listofnotes');
+  //       element.innerHTML = viewdanotes.render();
+  //       clearTextBox();
+  //     };
+  //
+  //
+  //
+  //     NoteController.prototype.clearTextBox = function() {
+  //       document.getElementById('notebook').value = '';
+  //     };
+
+
+// NoteController.writeNote();
