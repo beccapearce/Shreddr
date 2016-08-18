113e01d6b269fdad2f0023e717a9ccc10ce37f34
tchungnz <tchung.nz@gmail.com>
2016-08-18 10:40:32 +0100

    feature and unit tests all pass

--- test/feature-test.js
+++ test/feature-test.js
@@ -1,32 +1,28 @@
 console.log("feature test");
 
-var newNoteController = new NoteController();
 var newNoteList = new NoteList();
 var newNoteListView = new NoteListView(newNoteList);
 var yoda = new Yoda();
 
-
-function beforeFeatureTest() {
-  newNoteList.createNote("go team Shreddr! go team Shreddr! go team Shreddr!");
-  newNoteController.renderPage();
-}
-
 function testNoteListExists() {
-  yoda.testAnything(document.getElementById("app") !== null, testNoteListExists.name);
+  yoda.testAnything(document.getElementById("listofnotes") !== null, testNoteListExists.name);
 }
 
-function testNoteListValue() {
-  yoda.testAnything(document.getElementById("app").textContent.indexOf('go team') !== -1, testNoteListValue.name);
+function testCreateNote() {
+document.getElementById("notebook").value = "oat so simple, golden syrup. oat so simple! You're so posh you have a resting richface!";
+document.getElementById("submit").click();
+window.setTimeout(function () {
+yoda.testAnything(document.getElementById('listofnotes').textContent.indexOf('oat so simple') !== -1, testCreateNote.name);
+}, 1000);
 }
 
 function testClickNote() {
-  window.setTimeout(function () {
-  yoda.testAnything(document.getElementById('app').textContent.indexOf('go team Shreddr! go team Shreddr! go team Shreddr!') !== -1, testClickNote.name);
-  }, 1000);
   document.getElementById("0").click();
+  window.setTimeout(function () {
+  yoda.testAnything(document.getElementById('listofnotes').textContent.indexOf("oat so simple, golden syrup. oat so simple! You're so posh you have a resting richface!") !== -1, testClickNote.name);
+  }, 1000);
 }
 
-beforeFeatureTest();
 testNoteListExists();
-testNoteListValue();
+testCreateNote();
 testClickNote();
